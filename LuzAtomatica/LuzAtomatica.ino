8a8b2aa544890e2be732d1b68eee1fe29e690ed1
RoberCortez <robercortezv@gmail.com>
2020-08-30 22:29:47 -0500

    Nuevo Ejemplo

---
+++ LuzAtomatica/LuzAtomatica.ino
@@ -0,0 +1,69 @@
+/****************************************************************************************
+ Luz Automática
+
+  Activa o desactiva una salida digital (Led,Relay,etc) dependiendo del nivel de intensidad de luz presente en el sensor
+  
+ Conexiones:
+  ------------------------------------------------------------------------
+  M. BH1750-> Arduino Uno,Nano,Mini | Arduino Mega,DUE | Arduino Leonardo
+  ------------------------------------------------------------------------
+  VCC      ->   5V                  |   5V             |   5V
+  GND      ->   GND                 |   GND            |   GND
+  SCL      ->   A5                  |   21             |   3
+  SDA      ->   A4                  |   20             |   2 
+  ADRR     ->   NC                  |   NC             |   NC
+  ------------------------------------------------------------------------
+  *No se puede cambiar de pines, los pines indicados son para la comunicación I2C 
+   y son los pines SDA y SCL de cada placa respectivamente
+
+ Dirección del módulo I2C
+  a)Para ADRR no conectado o conectado a GND la dirección I2C es 0x23 
+  b)Para ADRR conectado a VCC la dirección I2C es 0x5C 
+ 
+ Desarrollado por Naylamp Mechatronics bajo licencia MIT (puede copiar, modificar, integrar o redistribuir)
+ 
+ Última modificación el 30/08/2020
+
+ Más información en:
+   -https://naylampmechatronics.com/blog/44_Tutorial-m%C3%B3dulo-sensor-de-luz-BH1750.html
+   -https://github.com/naylampmechatronics/Ejemplos_BH1750
+
+**************************************************************************************************/
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 Luxometro(0x23); //para una dirección i2C de 0x23
+
+// definición del pin del LED
+int pinFoco=13;
+
+// definición de variables
+uint16_t Lux_noche=20; //por debajo de este valor se enciende el foco
+uint16_t Lux_dia=30;  //por encima de este valor se apaga el
+
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Inicializando sensor...");
+  Luxometro.begin(BH1750::CONTINUOUS_HIGH_RES_MODE); //inicializamos el sensor
+  pinMode(pinFoco, OUTPUT);
+  digitalWrite(pinFoco, LOW);
+}
+
+void loop() {
+  uint16_t lux = Luxometro.readLightLevel();//Realizamos una lectura del sensor
+  Serial.print("Luz(iluminancia): ");
+  Serial.print(lux);
+  Serial.println(" lx");
+  if(lux<Lux_noche) 
+  {
+      digitalWrite(pinFoco, HIGH);
+      Serial.println(" Noche  - Foco Encendido");
+  }
+  else if(lux>Lux_dia)
+  {
+      digitalWrite(pinFoco, LOW);
+      Serial.println(" Dia - Foco Apagado");
+  }
+  delay(500);
+}
